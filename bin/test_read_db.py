70a07853328ed2e29d1b99e5266fa7a76746016d
FinnbarrOC <lukasocallahan@gmail.com>
2019-06-05 16:09:21 -0400

    Added convenient script to drop databases during testing

--- bin/test_read_db.py
+++ bin/test_read_db.py
@@ -2,22 +2,22 @@ import mongoengine
 import importlib
 from importlib import util
 
-spec = importlib.util.spec_from_file_location("schemas", "bin/categories/smt/schemas.py")
+spec = importlib.util.spec_from_file_location("schemas", "bin/categories/sat/schemas.py")
 schemas = importlib.util.module_from_spec(spec)
 spec.loader.exec_module(schemas)
 
 
 def main():
-    mongoengine.connect('smt_database')
+    mongoengine.connect('sat_database')
 
-    print("%d SMTInstances found" % len(schemas.SMTInstance.objects()))
-    print("%d SMTResults found" % len(schemas.SMTResult.objects()))
+    print("%d SATInstances found" % len(schemas.SATInstance.objects()))
+    print("%d SATResults found" % len(schemas.SATResult.objects()))
 
-    for SMTInstance in schemas.SMTInstance.objects():
-        print(SMTInstance.to_json())
+    for SATInstance in schemas.SATInstance.objects():
+        print(SATInstance.to_json())
 
-    for SMTResult in schemas.SMTResult.objects():
-        print(SMTResult.to_json())
+    for SATResult in schemas.SATResult.objects():
+        print(SATResult.to_json())
 
 
 if __name__ == '__main__':
