a3aa05d4aed4ca5586b08775cca1f3b9ec75cdcc
FinnbarrOC <lukasocallahan@gmail.com>
2019-06-11 11:05:49 -0400

    Added result aggregation for instances

--- bin/sat/schemas.py
+++ bin/sat/schemas.py
@@ -12,6 +12,9 @@ CONFIG = json.loads(open("bin/config.json", 'r').read())
 
 class Instance(Document):
     filename = StringField(required=True)
+    num_sat = IntField()
+    num_unsat = IntField()
+    num_unknown = IntField()
 
     meta = {
         'indexes': [
@@ -22,28 +25,15 @@ class Instance(Document):
 
 class Result(Document):
     program = StringField(required=True)
-    nickname = StringField()
-    # command = StringField()
+    nickname = StringField(required=True)
     # version = StringField()
     instance = ReferenceField(Instance, required=True)
     result = StringField(required=True)
     elapsed = FloatField(required=True)
 
 
-# Parses all unique instances and writes them to the database
-def write_instances(instances):
-    mongoengine.connect(CONFIG["database_name"])
-
-    for instance in instances:
-        stripped_instance = instance.split("/", 1)[1]
-
-        if not Instance.objects(filename=stripped_instance):
-            Instance.objects(filename=stripped_instance).update_one(upsert=True, set__filename=stripped_instance)
-
-    mongoengine.connection.disconnect()
-
-
-# Function to parse data for analyze from the database
+# Function to parse data for analyze from the database:
+# ------------------------------------------------------
 def read_database():
     data = {}
 
@@ -51,11 +41,24 @@ def read_database():
     parsed_result = np.dtype([('Instance', '<U14'), ('Result', '<U7'), ('Time', '<f8')])
     for result in Result.objects():
 
+        # Formats data for analyze
         new_data = np.array([(result.instance.filename, result.result, result.elapsed)], dtype=parsed_result)
 
         if result.nickname in data:
             data[result.nickname] = np.append(data[result.nickname], new_data)
         else:
             data[result.nickname] = new_data
+
+        # Updates the current instance's counters with each result
+        cur_instance = Instance.objects.get(filename=result.instance.filename)
+        if result.result == 'sat':
+            Instance.objects(filename=result.instance.filename).\
+                update_one(set__num_sat=cur_instance.num_sat + 1)
+        elif result.result == 'unsat':
+            Instance.objects(filename=result.instance.filename).\
+                update_one(set__num_unsat=cur_instance.num_unsat + 1)
+        else:
+            Instance.objects(filename=result.instance.filename).\
+                update_one(set__num_unknown=cur_instance.num_unknown + 1)
 
     return data
