6511bb915fa06950473839901170af808de40a60
FinnbarrOC <lukasocallahan@gmail.com>
2019-06-26 12:50:31 -0400

    Restructured for SAT feature extraction

---
+++ categories/sat/tools/syrup/syrup/syrup/parallel/depend.mk
@@ -0,0 +1,189 @@
+/home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/SolverConfiguration.o /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/SolverConfiguration.or /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/SolverConfiguration.od /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/SolverConfiguration.op: \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/SolverConfiguration.cc \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../parallel/MultiSolvers.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../parallel/ParallelSolver.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../core/SolverTypes.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../mtl/IntTypes.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../mtl/Alg.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../mtl/Vec.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../mtl/XAlloc.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../mtl/Map.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../mtl/Alloc.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../core/Solver.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../mtl/Heap.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../utils/Options.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../utils/ParseUtils.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../core/BoundedQueue.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../core/Constants.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../mtl/Clone.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../core/SolverStats.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../simp/SimpSolver.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../mtl/Queue.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../parallel/SharedCompanion.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../parallel/SolverCompanion.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../parallel/ClausesBuffer.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../parallel/SolverConfiguration.h
+/home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/MultiSolvers.o /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/MultiSolvers.or /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/MultiSolvers.od /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/MultiSolvers.op: \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/MultiSolvers.cc \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../parallel/MultiSolvers.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../parallel/ParallelSolver.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../core/SolverTypes.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../mtl/IntTypes.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../mtl/Alg.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../mtl/Vec.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../mtl/XAlloc.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../mtl/Map.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../mtl/Alloc.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../core/Solver.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../mtl/Heap.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../utils/Options.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../utils/ParseUtils.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../core/BoundedQueue.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../core/Constants.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../mtl/Clone.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../core/SolverStats.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../simp/SimpSolver.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../mtl/Queue.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../parallel/SharedCompanion.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../parallel/SolverCompanion.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../parallel/ClausesBuffer.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../mtl/Sort.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../utils/System.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../parallel/SolverConfiguration.h
+/home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/SharedCompanion.o /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/SharedCompanion.or /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/SharedCompanion.od /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/SharedCompanion.op: \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/SharedCompanion.cc \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../core/Solver.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../mtl/Heap.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../mtl/Vec.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../mtl/IntTypes.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../mtl/XAlloc.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../mtl/Alg.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../utils/Options.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../utils/ParseUtils.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../core/SolverTypes.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../mtl/Map.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../mtl/Alloc.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../core/BoundedQueue.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../core/Constants.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../mtl/Clone.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../core/SolverStats.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../parallel/ParallelSolver.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../simp/SimpSolver.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../mtl/Queue.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../parallel/SharedCompanion.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../parallel/SolverCompanion.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../parallel/ClausesBuffer.h
+/home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/ClausesBuffer.o /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/ClausesBuffer.or /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/ClausesBuffer.od /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/ClausesBuffer.op: \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/ClausesBuffer.cc \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../parallel/ClausesBuffer.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../mtl/Vec.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../mtl/IntTypes.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../mtl/XAlloc.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../core/SolverTypes.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../mtl/Alg.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../mtl/Map.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../mtl/Alloc.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../core/Solver.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../mtl/Heap.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../utils/Options.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../utils/ParseUtils.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../core/BoundedQueue.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../core/Constants.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../mtl/Clone.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../core/SolverStats.h
+/home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/ParallelSolver.o /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/ParallelSolver.or /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/ParallelSolver.od /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/ParallelSolver.op: \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/ParallelSolver.cc \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../parallel/ParallelSolver.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../core/SolverTypes.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../mtl/IntTypes.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../mtl/Alg.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../mtl/Vec.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../mtl/XAlloc.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../mtl/Map.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../mtl/Alloc.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../core/Solver.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../mtl/Heap.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../utils/Options.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../utils/ParseUtils.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../core/BoundedQueue.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../core/Constants.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../mtl/Clone.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../core/SolverStats.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../simp/SimpSolver.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../mtl/Queue.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../parallel/SharedCompanion.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../parallel/SolverCompanion.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../parallel/ClausesBuffer.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../mtl/Sort.h
+/home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/SolverCompanion.o /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/SolverCompanion.or /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/SolverCompanion.od /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/SolverCompanion.op: \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/SolverCompanion.cc \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../parallel/SolverCompanion.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../mtl/Vec.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../mtl/IntTypes.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../mtl/XAlloc.h
+/home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/Main.o /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/Main.or /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/Main.od /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/Main.op: \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/Main.cc \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../utils/System.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../mtl/IntTypes.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../utils/ParseUtils.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../utils/Options.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../mtl/Vec.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../mtl/XAlloc.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../core/Dimacs.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../core/SolverTypes.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../mtl/Alg.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../mtl/Map.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../mtl/Alloc.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../simp/SimpSolver.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../mtl/Queue.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../core/Solver.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../mtl/Heap.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../core/BoundedQueue.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../core/Constants.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../mtl/Clone.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../core/SolverStats.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../parallel/ParallelSolver.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../parallel/SharedCompanion.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../parallel/SolverCompanion.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../parallel/ClausesBuffer.h \
+ /home/ocallaha/PycharmProjects/aiutare/tools/sat/syrup/syrup/syrup/parallel/../parallel/MultiSolvers.h
+/Users/audemard/Sat/Src/syrup/simp/Main.o /Users/audemard/Sat/Src/syrup/simp/Main.or /Users/audemard/Sat/Src/syrup/simp/Main.od /Users/audemard/Sat/Src/syrup/simp/Main.op: /Users/audemard/Sat/Src/syrup/simp/Main.cc \
+  /Users/audemard/Sat/Src/syrup/simp/../utils/System.h \
+  /Users/audemard/Sat/Src/syrup/simp/../mtl/IntTypes.h \
+  /Users/audemard/Sat/Src/syrup/simp/../utils/ParseUtils.h \
+  /Users/audemard/Sat/Src/syrup/simp/../utils/Options.h \
+  /Users/audemard/Sat/Src/syrup/simp/../mtl/Vec.h \
+  /Users/audemard/Sat/Src/syrup/simp/../mtl/XAlloc.h \
+  /Users/audemard/Sat/Src/syrup/simp/../core/Dimacs.h \
+  /Users/audemard/Sat/Src/syrup/simp/../core/SolverTypes.h \
+  /Users/audemard/Sat/Src/syrup/simp/../mtl/Alg.h \
+  /Users/audemard/Sat/Src/syrup/simp/../mtl/Map.h \
+  /Users/audemard/Sat/Src/syrup/simp/../mtl/Alloc.h \
+  /Users/audemard/Sat/Src/syrup/simp/../simp/SimpSolver.h \
+  /Users/audemard/Sat/Src/syrup/simp/../mtl/Queue.h \
+  /Users/audemard/Sat/Src/syrup/simp/../core/Solver.h \
+  /Users/audemard/Sat/Src/syrup/simp/../mtl/Heap.h \
+  /Users/audemard/Sat/Src/syrup/simp/../core/BoundedQueue.h \
+  /Users/audemard/Sat/Src/syrup/simp/../core/Constants.h \
+  /Users/audemard/Sat/Src/syrup/simp/../mtl/Clone.h \
+  /Users/audemard/Sat/Src/syrup/simp/../core/SolverStats.h
+/Users/audemard/Sat/Src/syrup/simp/SimpSolver.o /Users/audemard/Sat/Src/syrup/simp/SimpSolver.or /Users/audemard/Sat/Src/syrup/simp/SimpSolver.od /Users/audemard/Sat/Src/syrup/simp/SimpSolver.op: /Users/audemard/Sat/Src/syrup/simp/SimpSolver.cc \
+  /Users/audemard/Sat/Src/syrup/simp/../mtl/Sort.h \
+  /Users/audemard/Sat/Src/syrup/simp/../mtl/Vec.h \
+  /Users/audemard/Sat/Src/syrup/simp/../mtl/IntTypes.h \
+  /Users/audemard/Sat/Src/syrup/simp/../mtl/XAlloc.h \
+  /Users/audemard/Sat/Src/syrup/simp/../simp/SimpSolver.h \
+  /Users/audemard/Sat/Src/syrup/simp/../mtl/Queue.h \
+  /Users/audemard/Sat/Src/syrup/simp/../core/Solver.h \
+  /Users/audemard/Sat/Src/syrup/simp/../mtl/Heap.h \
+  /Users/audemard/Sat/Src/syrup/simp/../mtl/Alg.h \
+  /Users/audemard/Sat/Src/syrup/simp/../utils/Options.h \
+  /Users/audemard/Sat/Src/syrup/simp/../utils/ParseUtils.h \
+  /Users/audemard/Sat/Src/syrup/simp/../core/SolverTypes.h \
+  /Users/audemard/Sat/Src/syrup/simp/../mtl/Map.h \
+  /Users/audemard/Sat/Src/syrup/simp/../mtl/Alloc.h \
+  /Users/audemard/Sat/Src/syrup/simp/../core/BoundedQueue.h \
+  /Users/audemard/Sat/Src/syrup/simp/../core/Constants.h \
+  /Users/audemard/Sat/Src/syrup/simp/../mtl/Clone.h \
+  /Users/audemard/Sat/Src/syrup/simp/../core/SolverStats.h \
+  /Users/audemard/Sat/Src/syrup/simp/../utils/System.h
