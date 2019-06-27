7cce00485628b5dcae868c111199c7407dfc92ef
David Tang <43974221+itangdav@users.noreply.github.com>
2019-06-27 10:52:13 -0400

    Scatterplot of time vs num of propagations

---
+++ bin/test_scatterplot.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+import importlib
+from subprocess import Popen
+import mongoengine
+import plotly
+import plotly.graph_objs as go
+from bin.config import config
+
+Popen("mongod --dbpath ./results --logpath ./results/log/mongodb.log".split() +
+      " --replSet monitoring_replSet".split())
+
+schemas = importlib.import_module(config["schemas"])
+mongoengine.connect(config["database_name"], replicaset="monitoring_replSet")
+
+include_sat = True
+include_unsat = True
+include_timeout = True
+
+names = ["syrup_stock", 'minisat_stock', 'minisat_clone', 'maple_stock']
+
+y_coords = []
+
+x_coords = []
+
+for i in range(4):
+    x_coords.append([])
+    y_coords.append([])
+
+
+for result in schemas.Result.objects():
+    index = names.index(result.nickname)
+    if (result.result == "sat" and include_sat) or (result.result == "unsat" and include_unsat) or (result.elapsed ==
+    config["timeout"] and include_timeout):
+        y_coords[index].append(result.elapsed)
+        x_coords[index].append(result.num_propagations)
+
+data = [None]*len(names)
+
+for i in range(len(names)):
+    data[i] = go.Scatter(
+        x=x_coords[i],
+        y=y_coords[i],
+        mode='markers',
+        name=names[i]
+    )
+
+# Finish and create graph
+# layout = go.Layout(
+#     barmode='group'
+# )
+
+plotly.offline.plot({
+    "data": data,
+    # "layout": layout
+}, auto_open=True, filename="images/testerScatter.html")
+
+mongoengine.connection.disconnect()
